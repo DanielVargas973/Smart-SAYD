85053f0aa04c0130d1326e3ee58c3a3d5bb2d081
Daniel <Daniel@Daniel-PC>
2017-05-20 19:53:53 -0500

    Cambio de calendario a datepicker(para que sea compatible con todos los buscadores)

--- Smart SAYD/web/js/fechas.js
+++ Smart SAYD/web/js/fechas.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    //llamar calendarios y definir formato en que reciben los datos
     $("#fecha1").datepicker({dateFormat:'yy/mm/dd'});
     $("#fecha2").datepicker({dateFormat:'yy/mm/dd'});
 });
