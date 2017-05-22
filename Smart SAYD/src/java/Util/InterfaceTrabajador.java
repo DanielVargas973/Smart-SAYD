993288185ec988e306c1ec3bcf2c2c50ce1b8e76
APRENDIZ <APRENDIZ@10.255.15.123>
2017-05-22 15:11:20 -0500

    Creacion Interface trabajador

---
+++ Smart SAYD/src/java/Util/InterfaceTrabajador.java
@@ -0,0 +1,7 @@
+package Util;
+public interface InterfaceTrabajador {
+    
+    public abstract boolean AgregarTrabajador();
+    public abstract boolean ActualizarTrabajador();
+    public abstract boolean EliminarTrabajador();
+}
