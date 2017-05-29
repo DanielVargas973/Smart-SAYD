d2dc07f07c7011016efa617194fe27c6336bb8cf
samuelandres7 <sacubillos5@misena.edu.co>
2017-05-28 19:59:51 -0500

    agregando registro programa

---
+++ Smart SAYD/src/java/Util/InterfacePrograma.java
@@ -0,0 +1,8 @@
+
+package Util;
+
+public interface InterfacePrograma {
+    public abstract boolean AgregarPrograma();
+    //public abstract boolean ActualizarCurso();
+    public abstract boolean EliminarPrograma();
+}
