514a7967af8d12feb3636986af3c982ccf99733c
Nicolas <Nicolas@Daniel-PC>
2017-06-04 19:17:27 -0500

    Inicio de implementacion sesiones(funcionalidad al cerrar sesion)

--- Smart SAYD/src/java/Modelo/DAO/DaoLogin.java
+++ Smart SAYD/src/java/Modelo/DAO/DaoLogin.java
@@ -11,23 +11,21 @@ import java.util.logging.Logger;
  * @author Daniel Vargas
  */
 public class DaoLogin extends Conexion {
+
     public Connection conn = null;
     private Statement puente = null;
     private ResultSet rs = null;
-    
-    public boolean encontrado = false;
-    public boolean listo = false;
-    
-    public String Numero_identificacion="";
-    public String Contrasena="";
-    public int Rol_Asignado =0;
-    
-    public DaoLogin (BeanLogin BLog){
+
+    public String Numero_identificacion = "";
+    public String Contrasena = "";
+    public int Rol_Asignado = 0;
+
+    public DaoLogin(BeanLogin BLog) {
         super();
         try {
             conn = this.ObtenerConexion();
             puente = conn.createStatement();
-            
+
             Numero_identificacion = BLog.getNumero_identificacion();
             Contrasena = BLog.getContrasena();
             Rol_Asignado = BLog.getRol_asignado();
@@ -35,20 +33,26 @@ public class DaoLogin extends Conexion {
             Logger.getLogger(DaoGestionCursos.class.getName()).log(Level.SEVERE, null, e);
         }
     }
+
     public int IngresarSistema(String Numero_identificacion, String Contrasena) {
-        try {            
-            rs = puente.executeQuery("select Rol_Asignado from personal WHERE Numero_identificacion = '"+Numero_identificacion+"' and Contrasena = '"+Contrasena+"'");
-            while(rs.next()){
+        try {
+            rs = puente.executeQuery("select Rol_Asignado from personal WHERE Numero_identificacion = '" + Numero_identificacion + "' and Contrasena = '" + Contrasena + "'");
+            while (rs.next()) {
                 Rol_Asignado = rs.getInt(1);
             }
-            if (Rol_Asignado==0) {
-            try {
-                puente.executeQuery("select * FROM estudiante where Numero_identificacion ='"+Numero_identificacion+"' and Contrasena ='"+Contrasena+"'" );       
-                Rol_Asignado= 4 ;
-            } catch (Exception e) {
-                Logger.getLogger(DaoGestionCursos.class.getName()).log(Level.SEVERE, null, e);
+            if (Rol_Asignado == 0) {
+                try {
+                    rs = puente.executeQuery("select idEstudiante FROM estudiante where Numero_identificacion ='" + Numero_identificacion + "' and Contrasena ='" + Contrasena + "'");
+                    while (rs.next()) {
+                        Rol_Asignado = rs.getInt(1);
+                    }
+                    if (Rol_Asignado != 0) {
+                        Rol_Asignado = 4;
+                    }
+                } catch (Exception e) {
+                    Logger.getLogger(DaoGestionCursos.class.getName()).log(Level.SEVERE, null, e);
+                }
             }
-        }
             conexion.close();
         } catch (Exception e) {
             Logger.getLogger(DaoGestionCursos.class.getName()).log(Level.SEVERE, null, e);
